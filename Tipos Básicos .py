fe607daa186970a81e55ae4405471bc11d1bddde
ericasnogueira <ericasnogueira10@gmail.com>
2021-11-09 10:41:07 -0300

    Create Tipos Básicos .py

---
+++ Tipos Básicos .py
@@ -0,0 +1,25 @@
+#Tipos Básicos
+print(True)
+print(False)
+print(1 * 2 +1)
+print('aqui eu falo ')
+print("pode ser assim") 
+
+#repetindo o 'muito' 3 vezes
+print('voce e' + 3  * ' muito ' + ' legal!' )
+#print(3 + '3' ) -> ambiguidade
+
+#lista
+lista = [1 , 2 ,3]
+
+#dicionário (chave e valor)
+
+dicionário  = {'nome ' : 'pedro', 'idade ' : 22}
+
+#exibindo lista e dicionário
+print(lista)
+print(dicionário)
+
+#none é a ausência de valor
+#ex:
+None
