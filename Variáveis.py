912d0a57030cbd7e0fcaa5776680a02a8911edff
ericasnogueira <ericasnogueira10@gmail.com>
2021-11-09 13:05:15 -0300

    Create Variáveis.py

---
+++ Variáveis.py
@@ -0,0 +1,32 @@
+#Variáveis
+a = 10
+b = 5.2
+
+print(a + b)
+
+a = 'agora sou uma string'
+print(a )
+print(b) 
+
+#print(a + b) -> não ira pq a mão é mais 10 e sim uma string  
+
+
+
+#comentários
+ 
+salario = 3450.45
+despesas = 2456.2 
+saldo = salario - despesas
+print( saldo)
+print('meus salario e : ' ,salario)
+print('minhas contas sao : ', despesas)
+print('vou ficar com : ' ,saldo)
+print('fim de verdade ')
+
+""""
+asddasdasdasdasdas
+"""
+
+#para dizer se está em um ambiente de teste ou não
+teste = True 
+print('estou ? ' , teste)
