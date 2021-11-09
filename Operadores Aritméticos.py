9b5aaef924c69034400777627057fb0d682e94b2
ericasnogueira <ericasnogueira10@gmail.com>
2021-11-09 13:44:35 -0300

    Create Operadores Aritméticos.py

---
+++ Operadores Aritméticos.py
@@ -0,0 +1,24 @@
+
+print(2+3)
+
+print(4-7)
+
+print(2*5.3)
+
+print(9.4 / 3)
+
+#o resulto dara um número inteiro 
+print(9.4 // 3)
+ 
+#elevado a oitava potência
+print(2 ** 8 )
+print(2 * 2 * 2 * 2 * 2 * 2 * 2 * 2)
+
+#resto da divisão (modulo)
+print( 10 % 3)
+
+a = 12
+b = a 
+print (a+b)
+
+print(a)
