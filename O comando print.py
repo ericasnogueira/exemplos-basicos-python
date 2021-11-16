21d8ca67464a74bb42a048111419847f7b1893f4
ericasnogueira <ericasnogueira10@gmail.com>
2021-11-16 12:23:18 -0300

    Create O comando print.py

---
+++ O comando print.py
@@ -0,0 +1,23 @@
+
+#falando para o print para usa o sep usar como separador
+print('luiz', 'otavio', sep='-')
+
+print('Joao', 'e' , 'Maria ', sep='-')#com quebra de linha
+
+#sem quebra de linha, tudo junto
+print('luiz', 'otavio', sep='-', end=' ')
+
+print('Joao', 'e' , 'Maria ', sep='-')
+
+#outro jeito com end
+
+print('luiz', 'otavi', sep='-', end='######')
+
+print('Joao', 'e' , 'Maria ', sep='-')
+
+#840.176.070-18
+print('840','176','070',sep='.',end='-')
+
+print('18')
+
+
