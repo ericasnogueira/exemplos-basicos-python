f295aec5453665cdc0d270900fce6f6e475437a3
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-08 10:41:00 -0300

    Create Desafio_prático.py

---
+++ Desafio_prático.py
@@ -0,0 +1,13 @@
+
+
+nome = 'Luiz'
+idade = 34
+ano_atual = 2021
+altura = 1.8
+peso = 80
+imc = peso / (altura ** 2)
+nascimento = ano_atual - idade
+
+print(f'{nome} tem {idade} anos, {altura} de altura e pesa {peso}kg.')
+print(f'o IMC de {nome} e {imc:.2f}.')
+print(f'{nome} nasceu em {nascimento}')
