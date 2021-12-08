b6924673bd518731e9b11652108ee1d2e57fdbd0
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-08 09:16:48 -0300

    Update Variáveis.py

--- Variáveis.py
+++ Variáveis.py
@@ -30,3 +30,35 @@ asddasdasdasdasdas
 #para dizer se está em um ambiente de teste ou não
 teste = True 
 print('estou ? ' , teste)
+
+
+# INICIAR COM LETRA, PODE CONTER NÚMEROS, SEPARAR _, LETRAS MINÚSCULAS
+
+nome = 'erica'
+idade = 20 #INT
+altura = 1.64 #FLOAT
+e_maior = idade > 18 #bool 
+peso = 42
+
+#outro exemplo 
+data_1 = False 
+data_2= True
+
+print('Nome : ' , nome)
+print('idade : ' , idade)
+print('altura : ' , altura)
+print('e maior de idade : ' , e_maior)
+
+print(altura * idade)
+
+# a divisão sera inteira com // 
+imc = peso // (altura * altura)
+
+#imc
+print( nome, 'tem ' , idade ,' anos de idade e o seu imc e', imc)
+
+
+
+
+#type para ve o valor da variável
+print(nome, type(nome))
