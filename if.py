d5e46379e02cc21e7990f1ce98a4e81f704523af
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-09 11:30:48 -0300

    Update if.py

--- if.py
+++ if.py
@@ -1,3 +1,41 @@
+
+
+#se
+if False:
+      print('VERDADEIRO.')
+#se não 
+elif  False:
+      print(' AGORA É VERDADEIRO.')
+elif False:
+      print("MAIS UMA VERDADEIRA.")
+#senão
+else : 
+      print("NÃO É VERDADEIRA")
+      
+#outro jeito
+
+if False:
+      print('VERDADEIRO.')
+      print('teste teste 2')
+      
+elif  True:
+      print(' AGORA É VERDADEIRO')
+      nome = input('QUAL É O SEU NOME ?')
+      
+      print(f'seu nome é {nome}')
+      
+elif False:
+      print("MAIS UMA VERDADEIRA.")
+      print(22+22)
+
+else : 
+      print("NÃO É VERDADEIRA")
+      print('ola')
+
+
+
+
+
 car ='Audi'
 car2 ='AUDI'
 
