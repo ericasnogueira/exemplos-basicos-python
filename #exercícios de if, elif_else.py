9a81c5d8248ebde722c36749b59ef6caaef2d544
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-10 10:17:56 -0300

    Create #exercícios de if, elif_else.py

---
+++ #exercícios de if, elif_else.py
@@ -0,0 +1,14 @@
+#exercícios de if, elif_else
+
+"""
+Faça um script que peça um valor e mostre na tela se o valor é positivo ou negativo.
+"""
+
+valor1=int(input('VALOR 1 :'))
+
+if valor1 > 0:
+    print(f'{valor1} é positivo')
+elif valor1 < 0:
+    print(f'{valor1} é negativo')
+else :
+    print(f'{valor1} não é positivo nem negativo')
