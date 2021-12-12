10a4fb0f630629a63f9403644858047ce7461657
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-12 13:51:37 -0300

    Create listas(4).py

---
+++ listas(4).py
@@ -0,0 +1,10 @@
+#listas em Python
+#fatiamento
+#append , insert , pop, del , clear, extend , + 
+# min , max 
+#range
+
+l2 = ['String', True, 10, -20.5]
+
+for elem in l2:
+    print(f'O tipo de elem é {type(elem)} e seu valor é {elem}')
