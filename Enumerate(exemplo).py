f04c1495663ad07da19106da0318adced8e23b15
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-12 16:24:47 -0300

    Create Enumerate(exemplo).py

---
+++ Enumerate(exemplo).py
@@ -0,0 +1,11 @@
+"""
+Split, Join, Enumerate 
+*Split - dividir uma string #str
+*Join - juntar uma lista #str
+*Enumerate - Enumerar elementos da lista 
+"""
+
+lista = ['Luiz', 'João', 'João', 'Maria']
+
+for indice,nome in enumerate(lista):
+    print(indice,nome)
