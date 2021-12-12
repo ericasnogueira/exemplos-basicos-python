f5e1b7363fd8f116a8e25aeba0eff3637e9ab589
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-12 16:07:39 -0300

    Create Split(exemplo).py

---
+++ Split(exemplo).py
@@ -0,0 +1,14 @@
+"""
+Split, Join, Enumerate 
+*Split - dividir uma string #str
+*Join - juntar uma lista #str
+*Enumerate - Enumerar elementos da lista 
+"""
+
+string = 'O Brasil é o país do futebol, o Brasil é penta .'
+#usando o espaço pra dividir
+lista_1 = string.split(' ')
+lista_2=string.split(',')
+
+print(lista_1)
+print(lista_2)
