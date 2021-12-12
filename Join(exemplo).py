fe3ffa1573181dada3190c3fa109231e1032b383
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-12 16:14:31 -0300

    Create Join(exemplo).py

---
+++ Join(exemplo).py
@@ -0,0 +1,16 @@
+"""
+Split, Join, Enumerate 
+*Split - dividir uma string #str
+*Join - juntar uma lista #str
+*Enumerate - Enumerar elementos da lista 
+"""
+
+string = 'O Brasil é penta'
+lista =string.split(' ')
+string_2 = ','.join(lista)
+string_1 = ' '.join(lista)
+print(string)
+print(lista)
+print(string_1)
+print(string_2)
+
