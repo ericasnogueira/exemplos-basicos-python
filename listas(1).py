9f8b38fa9c390dc54e99cb0d1d8f1032d7303128
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-12 12:05:34 -0300

    Create listas(1).py

---
+++ listas(1).py
@@ -0,0 +1,18 @@
+#listas em Python
+#fatiamento
+#append , insert , pop, del , clear, extend , + 
+# min , max 
+#range
+
+
+#lista pode ter varios valores
+      # 0   1   2   3   4   5
+lista=['A','B','C','D','E', 10.5]
+     # -5  -4  -3  -2  -1
+#modificando o que esta dentro da lista 
+lista[5] = 'erica'
+
+print(lista[5])
+
+#fatiamento
+print(lista[:3])
