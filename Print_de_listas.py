405b80ef1fc2e1a137796e329b7dc4966b459072
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-21 11:10:18 -0300

    Create Print_de_listas.py

---
+++ Print_de_listas.py
@@ -0,0 +1,9 @@
+#Print de listas 
+#print 'normal'
+#método join -> texto.join(lista)
+
+produto = ['apple tv','mac','iphone x ','iphone 11','Ipad','apple watch','mac book','airpods']
+
+print('\n'.join(produto)) #cada um em uma linha 
+print('--'.join(produto))
+
