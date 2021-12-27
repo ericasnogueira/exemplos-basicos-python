2e38f7889a012ac465dc7f0cc22eaf8d1696e740
ericasnogueira <ericasnogueira10@gmail.com>
2021-12-27 10:12:54 -0300

    Create Exercícios_de_for(2).py

---
+++ Exercícios_de_for(2).py
@@ -0,0 +1,24 @@
+"""
+## 2. Análise de Vendas
+
+Nesse exercício vamos fazer uma "análise simples" de atingimento de Meta.
+
+Temos uma lista com os vendedores e os valores de vendas e queremos identificar (printar) quais os 
+vendedores que bateram a meta e qual foi o valor que eles venderam.
+"""
+
+meta = 10000
+vendas = [
+    #  0       1 
+    ['João', 15000],
+    ['Julia', 27000],
+    ['Marcus', 9900],
+    ['Maria', 3750],
+    ['Ana', 10300],
+    ['Alon', 7870],
+]
+
+for item in vendas:
+    if item [1] >= meta:
+        print(f'Vendedor {item[0]} bateu a meta. Fez {item[1]} vendas')
+print()
