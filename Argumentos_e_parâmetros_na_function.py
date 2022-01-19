886ab55c4e1eb11eb4372c63556756ab36ca095f
ericasnogueira <ericasnogueira10@gmail.com>
2022-01-19 11:07:43 -0300

    Create Argumentos_e_parâmetros_na_function.py

---
+++ Argumentos_e_parâmetros_na_function.py
@@ -0,0 +1,23 @@
+"""
+Argumentos/Parâmetros da Função
+
+
+    Estrutura:
+    def minha_funcao(parametro1, parametro2, parametro3):
+        return parametro1 + paramtro2 + parametro3
+"""
+#exemplo: nosso famoso print
+vendas = 50
+
+print('produto', 'Iphone','Vendas', vendas, 10 , 'unidades') 
+
+
+#só para mostrar o funcionamento, vamos criar uma função soma 
+
+def minha_soma(num1, num2, num3):
+    return num1+ num2 + num3
+
+
+soma = minha_soma(10, 20, 30)
+
+print(soma)
