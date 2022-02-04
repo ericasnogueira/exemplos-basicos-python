4672dd55686be30a9c9bbe40e2c2afe178cf4084
ericasnogueira <ericasnogueira10@gmail.com>
2022-02-04 08:46:43 -0300

    Create Desafio_de_contadores.py

---
+++ Desafio_de_contadores.py
@@ -0,0 +1,17 @@
+"""
+Desafio de contadores 
+
+0 10
+1 9
+2 8
+3 7
+4 6
+5 5
+6 4
+7 3
+8 2
+"""
+
+                     #comeca no 10 e termina no 2      
+for p, r in enumerate(range(10,1,-1)):
+    print(p,r)
